d33ed122012611d68e8497674196f8ca8ff0e5e7
ikechukwu eze <ikechukwu.e@credpal.com>
2022-05-04 13:41:40 +0100

    override list method to eliminate n+1 problem

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.db.models import OuterRef, Subquery
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Reservation
 from .serializers import ReservationSerializer
 
@@ -9,5 +11,20 @@ def reservations_page(request):
 
 
 class ReservationAPIView(generics.ListAPIView):
-    queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        return Reservation.objects.select_related("rental")
+    
+    def list(self, request):
+        query = self.get_queryset().annotate(
+            previous_reservation=Subquery(
+                Reservation.objects
+                .filter(rental=OuterRef('rental'))
+                .exclude(checkout__gte=OuterRef('checkout'))
+                .order_by('-checkout')
+                .values('reservation_id')[:1]
+            )
+        )
+        serializer = ReservationSerializer(query, many=True)
+        return Response(serializer.data)
